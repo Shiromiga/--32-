359046d5dfd2bd0e832c35e57aa66b5f4cc785c1
Shiromiga <141715923+Shiromiga@users.noreply.github.com>
2023-09-29 19:05:32 +0300

    Delete 4 практика directory

--- 4 практика/1 номер.py
+++
@@ -1,4 +0,0 @@
-s = int(input("Введите число 1 "))
-e = int(input("Введите число 2 "))
-for i in range(s, e+1):
-    print(i)
--- 4 практика/5 номер.py
+++
@@ -1,5 +0,0 @@
-n = int(input("Введите число "))
-s = 0
-for i in range(1, n+1):
-    s +=i**3
-print(s)
