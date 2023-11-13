3ccd217517062da443a1b75dee308cc38989ae38
Shiromiga <141715923+Shiromiga@users.noreply.github.com>
2023-11-13 17:01:04 +0300

    Update 7 практика  3 вариант.py

--- Практика 7/7 практика  3 вариант.py
+++ Практика 7/7 практика  3 вариант.py
@@ -1,19 +1,22 @@
 #Варинат 3
+
+
+
 # 1 Задание
 import math
-def gipotenuza(x, y):
-    return math.sqrt(x ** 2 + y ** 2)  # вычисляем гипотенузу
+def f(x, y):
+    return math.sqrt(x ** 2 + y ** 2)
 
-st1, str1 = map(int, input('Катеты 1-го треугольника st1,str1 = ').split())
-st2, str2 = map(int, input('Катеты 2-го треугольника st2, str2 = ').split())
-ca1 = gipotenuza(st1, str1)
-ca2 = gipotenuza(st1, str2)
+st1, str1 = map(int, input('Катеты 1 треугольника st1,str1 = ').split())
+st2, str2 = map(int, input('Катеты 2 треугольника st2, str2 = ').split())
+ca1 = f(st1, str1)
+ca2 = f(st1, str2)
 if ca1 == ca2:
     print('Гипотенузы равны')
 if ca1 > ca2:
-    print('Гипотенуза 1-го треугольника больше')
+    print('Гипотенуза 1 треугольника больше')
 if ca1 < ca2:
-    print('Гипотенуза 2-го треугольника больше')
+    print('Гипотенуза 2 треугольника больше')
 
 
 # Задание 2
