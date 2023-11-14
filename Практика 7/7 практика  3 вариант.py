77e3a2a24c4b62767a02d6175176c2a6078f0e03
Shiromiga <141715923+Shiromiga@users.noreply.github.com>
2023-11-14 18:54:44 +0300

    Update 7 практика  3 вариант.py

--- Практика 7/7 практика  3 вариант.py
+++ Практика 7/7 практика  3 вариант.py
@@ -1,11 +1,11 @@
-#Варинат 3
+#7 практика Варинат 3
 
 
 
 # 1 Задание
 import math
 def f(x, y):
-    return math.sqrt(x ** 2 + y ** 2)
+    return math.sqrt(x ** 2 + y ** 2) # Формула для нахождения гипотенузы
 
 st1, str1 = map(int, input('Катеты 1 треугольника st1,str1 = ').split())
 st2, str2 = map(int, input('Катеты 2 треугольника st2, str2 = ').split())
@@ -22,5 +22,5 @@ if ca1 < ca2:
 # Задание 2
 e = input('').lower().split(' ')
 for i in range(len(e)):
-    e[i] = ''.join(sorted(e[i]))
+    e[i] = ''.join(sorted(e[i])) # меняет букву на отсортированную 
 print(' '.join(e))
